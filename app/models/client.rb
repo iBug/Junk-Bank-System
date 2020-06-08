4d7e4b3608e0406bf726f7e6f460c8c2eb9a08af
iBug <git@ibugone.com>
2020-06-08 15:51:35 +0800

    Client form

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,9 @@
 class Client < Person
   belongs_to :manager, class_name: :Staff
   has_one :contact, dependent: :destroy
-  accepts_nested_attributes_for :contact
   has_many :created_deposit_accounts
   has_many :created_check_accounts
   has_and_belongs_to_many :loans
+
+  accepts_nested_attributes_for :contact
 end
