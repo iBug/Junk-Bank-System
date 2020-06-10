cb33e485b1f8d54a7a342ee551246381af1370c8
iBug <git@ibugone.com>
2020-06-11 02:00:05 +0800

    Use single table for STI

--- db/migrate/20200525000009_create_check_accounts.rb
+++
@@ -1,8 +0,0 @@
-class CreateCheckAccounts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :check_accounts, comment: '支票账户' do |t|
-      t.references :account, index: true, foreign_key: { on_delete: :cascade }
-      t.decimal :withdraw_amount, precision: 12, scale: 2, comment: '透支额'
-    end
-  end
-end
