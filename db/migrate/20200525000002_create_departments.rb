46d4cb10c0c5ff27a5c7bd146ffc5b50644f9aff
iBug <git@ibugone.com>
2020-06-10 13:28:41 +0800

    Generate DepartmentsController

--- db/migrate/20200525000002_create_departments.rb
+++ db/migrate/20200525000002_create_departments.rb
@@ -1,7 +1,7 @@
 class CreateDepartments < ActiveRecord::Migration[6.0]
   def change
     create_table :departments, comment: '部门' do |t|
-      t.string :name, limit: 64, comment: '部门名称'
+      t.string :name, limit: 64, unique: true, comment: '部门名称'
       t.string :type, limit: 64, comment: '部门类型'
 
       t.timestamps
