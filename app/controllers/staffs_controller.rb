19465eef8ad316eaa6a7064adb6544a3ca86233d
iBug <git@ibugone.com>
2020-06-10 15:30:41 +0800

    Staff Done!

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,74 @@
+class StaffsController < ApplicationController
+  before_action :set_staff, only: [:show, :edit, :update, :destroy]
+
+  # GET /staffs
+  # GET /staffs.json
+  def index
+    @staffs = Staff.all
+  end
+
+  # GET /staffs/1
+  # GET /staffs/1.json
+  def show
+  end
+
+  # GET /staffs/new
+  def new
+    @staff = Staff.new
+  end
+
+  # GET /staffs/1/edit
+  def edit
+  end
+
+  # POST /staffs
+  # POST /staffs.json
+  def create
+    @staff = Staff.new(staff_params)
+
+    respond_to do |format|
+      if @staff.save
+        format.html { redirect_to @staff, notice: 'Staff was successfully created.' }
+        format.json { render :show, status: :created, location: @staff }
+      else
+        format.html { render :new }
+        format.json { render json: @staff.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /staffs/1
+  # PATCH/PUT /staffs/1.json
+  def update
+    respond_to do |format|
+      if @staff.update(staff_params)
+        format.html { redirect_to @staff, notice: 'Staff was successfully updated.' }
+        format.json { render :show, status: :ok, location: @staff }
+      else
+        format.html { render :edit }
+        format.json { render json: @staff.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /staffs/1
+  # DELETE /staffs/1.json
+  def destroy
+    @staff.destroy
+    respond_to do |format|
+      format.html { redirect_to staffs_url, notice: 'Staff was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_staff
+      @staff = Staff.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def staff_params
+      params.require(:staff).permit(:person_id, :name, :phone, :address, :start_date, :manager, :branch_id, :department_id)
+    end
+end
