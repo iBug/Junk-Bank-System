44806e9ba976f4f90a85e1615c3c51816c2b692c
iBug <git@ibugone.com>
2020-06-13 15:37:40 +0800

    Use Active Model hooks to update access time

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -27,7 +27,6 @@ class AccountsController < ApplicationController
     account_type = account_params[:accountable_type]
     typed_account = account_type.safe_constantize.new(account_params[:accountable_attributes])
     @account = typed_account.build_account(account_params)
-    @account.update_attribute :last_access, Time.now
 
     respond_to do |format|
       if @account.save
@@ -43,8 +42,6 @@ class AccountsController < ApplicationController
   # PATCH/PUT /accounts/1
   # PATCH/PUT /accounts/1.json
   def update
-    @account.update_attribute :last_access, Time.now
-
     respond_to do |format|
       if @account.update(account_params)
         format.html { redirect_to @account, notice: 'Account was successfully updated.' }
--- app/models/account.rb
+++ app/models/account.rb
@@ -7,8 +7,14 @@ class Account < ApplicationRecord
 
   accepts_nested_attributes_for :accountable
 
+  before_save :update_access_time
+
   # Credits: https://stackoverflow.com/a/32915379/5958455
   def build_accountable(params)
     self.accountable = accountable_type.safe_constantize.new params
+  end
+
+  def update_access_time
+    update_attribute :last_access, Time.now
   end
 end
