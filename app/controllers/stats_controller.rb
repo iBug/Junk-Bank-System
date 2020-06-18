95c73b066e8102d67c4b05e31d9f798fedb47704
iBug <git@ibugone.com>
2020-06-19 00:18:42 +0800

    Better-looking home page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,7 @@
 class StatsController < ApplicationController
   def home
+    @cards = [Branch, Department, Staff, Client, Account, Loan].zip \
+      %w[dollar-square department manager user credit-card debt]
   end
 
   def index
