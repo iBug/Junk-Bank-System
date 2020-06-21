42d9afd8fd01e9fadaf4fb00385900a0df2b7ede
iBug <git@ibugone.com>
2020-06-21 14:22:27 +0800

    Serve assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts = nil
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -20,6 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
