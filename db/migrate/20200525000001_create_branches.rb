5120542b88c42a1326e00c26cbed772dd612ddcf
iBug <git@ibugone.com>
2020-06-26 00:04:43 +0800

    Migration: Branch name is unique

--- db/migrate/20200525000001_create_branches.rb
+++ db/migrate/20200525000001_create_branches.rb
@@ -1,7 +1,7 @@
 class CreateBranches < ActiveRecord::Migration[6.0]
   def change
     create_table :branches, comment: '支行' do |t|
-      t.string :name, limit: 64, comment: '名称'
+      t.string :name, limit: 64, unique: true, comment: '名称'
       t.string :city, limit: 64, comment: '城市'
       t.decimal :assets, precision: 12, scale: 2, default: 0.0, comment: '资产'
 
--- db/migrate/20200525000006_create_accounts.rb
+++ db/migrate/20200525000006_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts, comment: '账户' do |t|
-      t.references :branch, index: true, foreign_key: { on_delete: :cascade }, comment: '开户支行'
+      t.references :branch, index: true, foreign_key: { on_delete: :restrict }, comment: '开户支行'
       t.references :accountable, null: false, polymorphic: true, comment: '类型账户ID'
       t.decimal :balance, precision: 12, scale: 2, default: 0.0, comment: '余额'
       t.date :open_date, default: 'CURRENT_TIMESTAMP', comment: '开户日期'
